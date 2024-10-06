80205c6a27d59a4fd04738aca4ce5a01f37aaea1
gourabofficial <gangulygourab3@gmail.com>
2024-10-06 23:30:07 +0530

    simply create the basic ui for todo

---
+++ TodoList-App/src/App.jsx
@@ -0,0 +1,16 @@
+import AppName from "./Components/AppName";
+import Mytask from "./Components/Mytask";
+import TodoInput from "./Components/TodoInput";
+
+function App() {
+  return (
+    <center class="container text-center">
+      <AppName />
+      <TodoInput />
+      <Mytask />
+
+    </center>
+  );
+}
+
+export default App;
---
+++ TodoList-App/src/Components/Mytask.jsx
@@ -0,0 +1,17 @@
+function Mytask() {
+  return (
+    <div class="container text-center">
+      <div class="row">
+        <div class="col-6">Sleeping 7 hours</div>
+        <div class="col-4">10/10/2024</div>
+        <div class="col-2">
+          <button type="button" class="btn btn-danger">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Mytask;
