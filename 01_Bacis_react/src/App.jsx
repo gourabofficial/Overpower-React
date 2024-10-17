174ee9b2230b79caef6d6bae51df705b466e16c5
gourabofficial <gangulygourab3@gmail.com>
2024-10-17 19:46:41 +0530

    Refactor project configuration files

---
+++ 01_Bacis_react/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <div className='p-4 w-full h-screen bg-zinc-700 text-white '>
+      <div className= " p-4 bg-red-500 w-44 h-32 rounded-xl text-black">
+      <h1>Learning Basic React</h1>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
