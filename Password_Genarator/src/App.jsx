7e1a343994f80fcc96c73dfc8fa04306f27499c0
gourabofficial <gangulygourab3@gmail.com>
2024-10-19 00:27:24 +0530

    1% done only

--- Password_Genarator/src/App.jsx
+++ Password_Genarator/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <>
-      <h1 className=' text-white  text-4xl text-center'>Password Genarator</h1>
+      <h1 className='text-white  text-4xl text-center'>Password Genarator</h1>
     </>
   )
 }
