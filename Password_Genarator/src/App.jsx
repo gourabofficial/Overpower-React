332e8af5414fa9c8fd94e046cb7f37f9b5ca45b0
gourabofficial <gangulygourab3@gmail.com>
2024-10-19 00:26:47 +0530

    Refactor project configuration files and add Tailwind CSS support

---
+++ Password_Genarator/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './index.css'
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className=' text-white  text-4xl text-center'>Password Genarator</h1>
+    </>
+  )
+}
+
+export default App
