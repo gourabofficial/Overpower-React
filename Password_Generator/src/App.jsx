8cc70e977ce3931cf7452699cc678badb42ab9c8
gourabofficial <gangulygourab3@gmail.com>
2024-10-20 01:06:43 +0530

    adding some styling and add Onclick copy to copid
    comple this one

--- Password_Generator/src/App.jsx
+++ Password_Generator/src/App.jsx
@@ -5,7 +5,8 @@ function App() {
   const [length, setLength] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setcharAllowed] = useState(false);
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState("")
+  const[copied,setCopied] = useState(false)
   const passwordRef = useRef(null)
 
   const passwordGenerator = useCallback(() => {
@@ -25,6 +26,12 @@ function App() {
   const copypassword = useCallback(() => {
     passwordRef.current?.select( )
     window.navigator.clipboard.writeText(password)
+    setCopied(true); 
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 1000);
+
   })
 
   useEffect(() => {
@@ -34,27 +41,35 @@ function App() {
 
   return (
     <>
-      
-      <div className="w-full max-w-fit mx-auto  text-orange-500
-       bg-gray-800 rounded-lg shadow-md px-4 my-8 h-52">
-        <h1 className="text-4xl font-bold text-white mb-8 p-2">Password Generator</h1>
+       
+      <div className="w-full max-w-fit  h-72 mx-auto  text-black
+       bg-gray-800 rounded-lg shadow-md px-4 my-8 ">
+        <h1 className="text-4xl  shadow-xl font-bold text-orange-400 mb-8 p-2">Password Generator</h1>
         <div className="flex rounded-lg overflow-hidden
        md-4">
           
           <input type="text"
-            value={password} className="outline-none w-full px-3 py-1"
+            value={password} className="outline-none font-bold w-full px-3 py-1"
             placeholder="Password" readOnly ref={ passwordRef}
           />
-          <button onClick={copypassword} className=" bg-blue-500 outline-none text-white py-0.5 px-3 shrink-0 ">copy</button>
+           <button
+            onClick={copypassword}
+            className={`bg-blue-500 outline-none text-white py-2 px-4 rounded-r-md transition
+               duration-300 ease-in-out transform hover:scale-105 ${
+              copied ? "bg-blue-900" : "hover:bg-blue-600"
+            }`} 
+          >
+            {copied ? "Copied!" : "Copy"} 
+          </button>
 
         </div>
 
         <div className='flex text-sm gap-x-2'> 
           <div className='flex items-center gap-x-1'>
 
-            <input type="range" min={8} max={100} value={length} className="font-bold  cursor-pointer"
+            <input type="range" min={8} max={100} value={length} className=""
               onChange={(e)=>{setLength(e.target.value)} } />
-            <label className=" text-bold text-xl p-4">Length:({length})</label>
+            <label className=" font-bold text-xl text-white p-4">Length:({length})</label>
           </div>
           <div className='flex items-center gap-x-1'>
           <input  type="checkbox"
@@ -63,10 +78,10 @@ function App() {
           onChange={() => {
               setNumberAllowed((prev) => !prev);
           }}/>
-         < label className="font-bold text-xl" htmlFor ="numberInput">Numbers</label>
+         < label className="font-bold text-xl  text-white" htmlFor ="numberInput">Numbers</label>
           </div>
-          <div className="flex items-center gap-x-1">
-          <input className="font-bold text-xl"
+          <div className=" p-4 flex items-center gap-x-1">
+          <input 
               type="checkbox"
               defaultChecked={charAllowed}
               id="characterInput"
@@ -74,7 +89,7 @@ function App() {
                   setcharAllowed((prev) => !prev )
               }}
           />
-          <label htmlFor="characterInput">Characters</label>
+          <label  className="font-bold text-xl text-white" htmlFor="characterInput">Characters</label>
       </div>
         </div>
         </div>
