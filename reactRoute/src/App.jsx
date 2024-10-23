270136ae431897a91b9174090eefe27758ddc601
gourabofficial <gangulygourab3@gmail.com>
2024-10-23 16:12:18 +0530

    create route for home,about,contact  working on this project

--- reactRoute/src/App.jsx
+++ reactRoute/src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Components/Header/Header'
+import Footer from './Components/Footer/Footer'
+import Home from './Components/Home/Home'
+import About from './Components/About/About'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className='bg-slate-500 text-black'>React Router testing </h1>
+     
     </>
   )
 }
